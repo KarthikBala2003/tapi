3a2934493eeacf2a4596b6608cc9c947f1c0e104
madavan <madavan.a2310@gmail.com>
2023-12-27 23:03:27 +0530

    latest-commit

--- static/script.js
+++ static/script.js
@@ -1,52 +1,30 @@
-<<<<<<< HEAD
-// Toggle submenus in the sidebar
 document.addEventListener("DOMContentLoaded", function () {
-    var dropdowns = document.getElementsByClassName("dropdown-btn");
-    for (var i = 0; i < dropdowns.length; i++) {
-        dropdowns[i].addEventListener("click", function () {
-            this.classList.toggle("active");
-            var dropdownContent = this.nextElementSibling;
-            if (dropdownContent.style.display === "block") {
-                dropdownContent.style.display = "none";
-            } else {
-                dropdownContent.style.display = "block";
+    const jsonDataFile = "/static/menu_data.json";
+    const menuList = document.getElementById("menuList");
+
+    fetch(jsonDataFile)
+        .then(response => response.json())
+        .then(jsonData => {
+            for (const category in jsonData) {
+                const categoryItem = document.createElement("li");
+                const link = document.createElement("a");
+                link.href = "#";
+                link.textContent = category;
+                categoryItem.appendChild(link);
+
+                const submenu = document.createElement("div");
+                submenu.classList.add("submenu");
+
+                for (const item of jsonData[category]) {
+                    const menuItem = document.createElement("a");
+                    menuItem.href = `/reference/${item}`;
+                    menuItem.textContent = item.replace(/_/g, ' ').toUpperCase();
+                    submenu.appendChild(menuItem);
+                }
+
+                categoryItem.appendChild(submenu);
+                menuList.appendChild(categoryItem);
             }
-        });
-    }
+        })
+        .catch(error => console.error("Error loading JSON data:", error));
 });
-=======
-$(document).ready(function () {
-    
-    let list = $('<li/>').appendTo('#menu1');
-
-    $('<a />')
-        .text('Features')
-        .attr('href', '#')
-        .appendTo(list);
-
-    
-    let sub_ul = $('<ul/>').appendTo(list);
-
-    
-    $.getJSON('/static/menu_data.json', function (jsonData) {
-        $.each(jsonData, function (category, subcategories) {
-            
-            let sub_li = $('<li/>').appendTo(sub_ul);
-            $('<a />')
-                .text(category + ' + ')
-                .attr('href', '#')
-                .appendTo(sub_li);
-
-            let sub_sub_ul = $('<ul/>').appendTo(sub_li);
-
-            $.each(subcategories, function (index, subcategory) {
-                let sub_sub_li = $('<li/>').appendTo(sub_sub_ul);
-                $('<a />')
-                    .text(subcategory)
-                    .attr('href', '#')
-                    .appendTo(sub_sub_li);
-            });
-        });
-    });
-});
->>>>>>> 597ff7c12305622436d0bb3879bede87afeb228d

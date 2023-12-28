af15c5e1b1ac0bf7ef91592bca0cb6aee288023b
Karthik Bala <KarthikBala2003@gmail.com>
2023-12-27 19:52:23 -0600

    File Encrypt and Decrypt code added

---
+++ samples/pytosh.py
@@ -0,0 +1,23 @@
+import subprocess
+
+def execute_bash_script(script_path, *args):
+    command = ['bash', script_path] + list(args)
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+
+    if process.returncode == 0:
+        return stdout.decode().strip()
+    else:
+        error_message = stderr.decode().strip()
+        raise Exception(f'Bash script execution failed: {error_message}')
+
+# Example usage
+script_path = '/d/DEV/GitHub/automation/POC/script1.sh'
+input_param1 = 'hello'
+input_param2 = 'world'
+
+try:
+    output = execute_bash_script(script_path, input_param1, input_param2)
+    print(output)
+except Exception as e:
+    print(f'Error: {str(e)}')
---
+++ samples/script1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+param1=$1
+param2=$2
+
+output="Hello, $param1 $param2!"
+echo $output
